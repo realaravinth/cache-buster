4f8c8c0ed0cf6c127db9d1ffb32887893fe4f1b8
realaravinth <realaravinth@batsense.net>
2021-04-08 16:40:54 +0000

    Deploying to gh-pages from  @ 09e44c0e28770bb42c7eddb88a6a476071e86863 🚀

---
+++ actix_example/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["file_exists",""],["main",""]]});
--- source-files.js
+++ source-files.js
@@ -1,3 +1,4 @@
 var N = null;var sourcesIndex = {};
+sourcesIndex["actix_example"] = {"name":"","files":["main.rs"]};
 sourcesIndex["cache_buster"] = {"name":"","files":["hash.rs","lib.rs","map.rs"]};
 createSourceSidebar();
